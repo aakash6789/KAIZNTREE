dc55168427668d8d95be4a8b6555e75d961b86eb
Aakash <aakash2506c@gmail.com>
2024-02-12 15:36:16 +0530

    ok

--- server/app.js
+++ server/app.js
@@ -9,6 +9,9 @@ app.use(express.json({limit:'16kb'}));
 app.use(express.urlencoded({extended:true, limit:'16kb'}));
 app.use(express.static('../public'));
 app.use(cookieParser());
+app.get('/',(req,res)=>{
+    res.send('<h1>Welcome, here is your assignmnet</h1>')
+})
 app.use('/api/v1/users',userRouter);
 app.use('/api/v1/items',itemRouter);
 export default app;
